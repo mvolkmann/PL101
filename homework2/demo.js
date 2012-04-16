1c02c8a1d3979943a845a5bafe57b56d4c4c5e47
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2012-04-15 19:37:22 -0500

    added demo.js

---
+++ homework2/demo.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var compile = require('lib/compile');
+
+var melody_mus = { tag: 'seq',
+  left: { tag: 'seq',
+    left: { tag: 'note', pitch: 'a4', dur: 250 },
+    right: { tag: 'note', pitch: 'b4', dur: 250 }
+  },
+  right: { tag: 'seq',
+    left: { tag: 'note', pitch: 'c4', dur: 500 },
+    right: { tag: 'note', pitch: 'd4', dur: 500 }
+  }
+};
+
+console.log(melody_mus);
+console.log(compile(melody_mus));
