88001d55c13e81a416fbf2f6fb196793b950984c
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2012-06-10 12:27:49 -0500

    finished step 4 of homework 10

--- homework10/lib/hare.js
+++ homework10/lib/hare.js
@@ -1,5 +1,38 @@
 'use strict';
 
+var compileExpr; // used before defined
+
+function compileCall(expr) {
+  //console.log('compileCall: expr =', expr);
+  var code = expr.name + '(';
+
+  var firstArg = true;
+  expr.args.forEach(function (arg) {
+    if (firstArg) {
+      firstArg = false;
+    } else {
+      code += ', ';
+    }
+    var tag = arg.tag;
+    var value = tag === 'ident' ? arg.name : arg;
+    code += compileExpr(value);
+  });
+
+  code += ')';
+  //console.log('compileCall: code =', code);
+  return code;
+}
+
+function compileEnvironment(env) {
+  var code = '';
+  env.forEach(function (arr) {
+    var name = arr[0];
+    var value = arr[1];
+    code += 'var ' + name + ' = ' + value + ';\n';
+  });
+  return code;
+}
+
 function compileExpr(expr) {
   if (typeof expr === 'number') {
     return expr.toString();
@@ -18,14 +51,31 @@ function compileExpr(expr) {
     return '(' + compileExpr(expr.left) + ') ' + tag + ' (' +
       compileExpr(expr.right) + ')';
 
+  case 'call':
+    return compileCall(expr);
+
+  case 'ident':
+    return expr.name;
+
   default:
     throw new Error('Unknown tag ' + expr.tag);
   }
 }
 
-function op(t, l, r) {
-  return { tag: t, left: l, right: r };
+function app(f, args) {
+  return {tag: 'call', name: f, args: args};
 }
 
+function op(t, l, r) {
+  return {tag: t, left: l, right: r};
+}
+
+function ref(n) {
+  return {tag: 'ident', name: n};
+}
+
+exports.app = app;
+exports.compileEnvironment = compileEnvironment;
 exports.compileExpr = compileExpr;
 exports.op = op;
+exports.ref = ref;
--- homework10/test/hare.mocha.js
+++ homework10/test/hare.mocha.js
@@ -3,8 +3,11 @@
 
 var assert = require('chai').assert;
 var hare = require('../lib/hare');
+var compileEnvironment = hare.compileEnvironment;
 var compileExpr = hare.compileExpr;
+var app = hare.app;
 var op = hare.op;
+var ref = hare.ref;
 
 test('compileExpr', function () {
   assert.equal(eval(compileExpr(op('+', 1, 1))), 2);
@@ -13,4 +16,47 @@ test('compileExpr', function () {
   assert.isTrue(eval(compileExpr(op('<', 1, 2))));
   assert.isFalse(
     eval(compileExpr(op('<', op('+', op('*', 2, 3), 1), op('*', 3, 1)))));
+});
+
+test('compileEnvironment', function () {
+  var env1 = [['x', 5], ['y', 100]];
+  var env2 = [
+    ['x', 7],
+    ['f', function (x) {
+      return 2 * x;
+    }]
+  ];
+
+  assert.equal(
+    eval(compileEnvironment(env1) + 'x'), 5);
+  assert.equal(
+    eval(compileEnvironment(env1) + 'y'), 100);
+  assert.equal(
+    eval(compileEnvironment(env2) + 'x'), 7);
+  assert.equal(
+    eval(compileEnvironment(env2) + 'f(x)'), 14);
+});
+
+test('compileCall', function () {
+  var x = 5;
+  function f(x) {
+    return 3 * x + 1;
+  }
+  function g() {
+    return 17;
+  }
+  function add(x, y) {
+    return x + y;
+  }
+
+  assert.equal(
+    eval(compileExpr(op('+', ref('x'), 1))), 6);
+  assert.equal(
+    eval(compileExpr(app('f', [3]))), 10);
+  assert.equal(
+    eval(compileExpr(app('g', []))), 17);
+  assert.equal(
+    eval(compileExpr(app('add', [2, 3]))), 5);
+  assert.equal(
+    eval(compileExpr(app('f', [app('f', [3])]))), 31);
 });
