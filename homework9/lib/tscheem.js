34a578556964b1e4f52569ad3ff5b379b1a515e2
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2012-06-10 10:48:46 -0500

    finished first seven parts of homework 9

--- homework9/lib/tscheem.js
+++ homework9/lib/tscheem.js
@@ -1,6 +1,15 @@
 'use strict';
 
 var prettyType; // used before defined
+var typeExpr; // used before defined
+
+function app2(f, x, y) {
+  return function (x, y) {
+    return function (y) {
+      return f(x)(y);
+    };
+  };
+}
 
 function arrow(left, right) {
   return {tag: 'arrowtype', left: left, right: right};
@@ -8,6 +17,19 @@ function arrow(left, right) {
 
 function base(name) {
   return {tag: 'basetype', name: name};
+}
+
+function lookup(env, name) {
+  var bindings = env.bindings;
+  if (bindings === undefined) {
+    throw new Error('environment is missing bindings property');
+  }
+
+  var value = bindings[name];
+  var outer = env.outer;
+  return value !== undefined ? value :
+    outer ? lookup(outer, name) :
+    undefined;
 }
 
 /*
@@ -36,6 +58,77 @@ function prettyType(type) {
   }
 }
 
+function sameType(a, b, env) {
+  return typeExpr(a, env) === typeExpr(b, env);
+}
+
+function typeExprIf(expr, env) {
+  var cond = expr[1];
+  //console.log('typeExprIf: cond =', cond);
+  var condType = typeExpr(cond, env);
+  //console.log('typeExprIf: condType =', condType);
+  if (condType.name !== 'bool') {
+    throw new Error('if requires a boolean condition');
+  }
+
+  var thenPart = expr[2];
+  //console.log('typeExprIf: thenPart =', thenPart);
+  var elsePart = expr[3];
+  //console.log('typeExprIf: elsePart =', elsePart);
+
+  var thenType = typeExpr(thenPart, env);
+  //console.log('typeExprIf: thenType =', thenType);
+  var elseType = typeExpr(elsePart, env);
+  //console.log('typeExprIf: elseType =', elseType);
+
+  return thenType.name === elseType.name ? thenType : undefined;
+}
+
+function typeExprArray(expr, env) {
+  var fnName = expr[0];
+  if (fnName === 'if') {
+    return typeExprIf(expr, env);
+  }
+
+  var arg = expr[1];
+  //console.log('callType: fnName =', fnName, ', arg =', arg);
+
+  var fnType = typeExpr(fnName, env);
+  var argType = typeExpr(arg, env);
+  //console.log('callType: fnType =', fnType);
+  //console.log('callType: argType =', argType);
+
+  if (fnType.tag !== 'arrowtype') {
+    throw new Error('not an arrow type');
+  }
+
+  // Verify argument type matches.
+  var paramType = fnType.left;
+  //console.log('callType: paramType =', paramType);
+  //console.log('callType: argType =', argType);
+  if (!sameType(paramType, argType)) {
+    throw new Error('argument type did not match parameter type');
+  }
+
+  var returnType = fnType.right;
+  return returnType;
+}
+
+function typeExpr(expr, env) {
+  var t = typeof expr;
+  //console.log('typeExpr: expr =', expr);
+  //console.log('typeExpr: t =', t);
+  return t === 'number' ?  {tag: 'basetype', name: 'num'} :
+    t === 'boolean' ?  {tag: 'basetype', name: 'bool'} :
+    t === 'string' ? lookup(env, expr) :
+    Array.isArray(expr) ? typeExprArray(expr, env) :
+    t === 'object' ? (expr.tag ? prettyType(expr) : t) :
+    undefined;
+}
+
+exports.app2 = app2;
 exports.arrow = arrow;
 exports.base = base;
 exports.prettyType = prettyType;
+exports.sameType = sameType;
+exports.typeExpr = typeExpr;
--- homework9/test/tscheem.mocha.js
+++ homework9/test/tscheem.mocha.js
@@ -1,12 +1,137 @@
 'use strict';
 var assert = require('chai').assert;
 var tscheem = require('../lib/tscheem');
+var app2 = tscheem.app2;
 var arrow = tscheem.arrow;
 var base = tscheem.base;
 var prettyType = tscheem.prettyType;
+var sameType = tscheem.sameType;
+var typeExpr = tscheem.typeExpr;
+
+test('curry', function () {
+  function plusCurry(x) {
+    return function (y) {
+        return x + y;
+      };
+  }
+
+  assert.typeOf(app2(plusCurry), 'function');
+  assert.typeOf(app2(plusCurry)(2), 'function');
+  assert.equal(app2(plusCurry)(2)(3), 5);
+});
 
 test('prettyType', function () {
   assert.equal(prettyType(base('foo')), 'foo');
   assert.equal(prettyType(arrow('foo', 'bar')), '(foo -> bar)');
   assert.equal(prettyType(arrow('foo', arrow('bar', 'baz'))), '(foo -> (bar -> baz))');
+});
+
+test('typeExpr', function () {
+  var b1 = base('num');
+  var b2 = base('atom');
+  var a1 = arrow(b1, b1);
+  var a2 = arrow(b1, b2);
+  var a3 = arrow(b2, b1);
+  var env = {};
+
+  assert.isTrue(sameType(b1, b1, env));
+  assert.isFalse(sameType(b1, b2, env));
+  assert.isFalse(sameType(b1, a1, env));
+  assert.isFalse(sameType(a1, b1, env));
+  assert.isTrue(sameType(a1, a1, env));
+  assert.isFalse(sameType(a1, a2, env));
+  assert.isFalse(sameType(a1, a3, env));
+});
+
+test('typeFunction', function () {
+  var context1 = {
+    bindings: {
+      'f': arrow(base('num'), arrow(base('num'), base('num'))),
+      'x': base('num')
+    }
+  };
+  var context2 = {
+    bindings: {
+      'f': arrow(arrow(base('num'), base('num')), base('num')),
+      'x': arrow(base('num'), base('num'))
+    }
+  };
+
+  function deepEqual(a, b) {
+    if (a === b) {
+      return true;
+    }
+
+    if (typeof a !== 'object' || typeof b !== 'object') {
+      return false;
+    }
+
+    var aKeys = Object.keys(a);
+    var bKeys = Object.keys(b);
+    if (aKeys.length !== bKeys.length) {
+      return false;
+    }
+
+    return aKeys.every(function (key) {
+      return deepEqual(a[key], b[key]);
+    });
+  }
+
+  function typeExprTester(env) {
+    var fType = typeExpr('f', env);
+    if (fType.tag !== 'arrowtype') {
+      return fType;
+    }
+
+    var paramType = fType.left;
+    var argType = typeExpr('x', env);
+    var returnType = fType.right;
+    //console.log('paramType =', paramType);
+    //console.log('argType =', argType);
+    //console.log('returnType =', returnType);
+
+    if (deepEqual(argType, paramType)) {
+      return returnType;
+    }
+
+    throw new Error('argument and parameter types do not match');
+  }
+
+  var expected = arrow(base('num'), base('num'));
+  assert.deepEqual(typeExprTester(context1), expected);
+
+  expected = base('num');
+  assert.deepEqual(typeExprTester(context2), expected);
+});
+
+test('typeIf', function () {
+  var context = {
+    bindings: {
+      '+': arrow(base('num'), arrow(base('num'), base('num'))),
+      '<': arrow(base('num'), arrow(base('num'), base('bool')))
+    }
+  };
+
+  function wrapExceptions(fn) {
+    return function () {
+      var args = Array.prototype.slice.call(arguments);
+      try {
+        return fn.apply(null, args);
+      } catch (e) {
+        console.log('wrapExceptions:', e);
+        return undefined;
+      }
+    };
+  }
+
+  typeExpr = wrapExceptions(typeExpr);
+
+  assert.deepEqual(typeExpr(['if', true, 3, 5], {}), base('num'));
+  assert.deepEqual(typeExpr(['if', true, true, 5], {}), undefined);
+  assert.deepEqual(typeExpr(['if', 3, 3, 5], {}), undefined);
+  assert.deepEqual(typeExpr([['+', 2], 3], context), base('num'));
+  assert.deepEqual(typeExpr([['<', 1], 2], context), base('bool'));
+  assert.deepEqual(
+    typeExpr(['if', [['<', 1], 2], [['+', 2], 3], 5], context),
+    base('num'));
 });
